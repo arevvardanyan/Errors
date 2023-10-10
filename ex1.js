671e0d9c24ee57229276c3c79ce1d7ffa2c98439
Arev <vardanyanarevik23@gmail.com>
2023-10-10 22:11:27 +0400

    String reverse

---
+++ ex1.js
@@ -0,0 +1,12 @@
+function reverseString(s) {
+    try {
+      const reversed = s.split('').reverse().join('');
+      console.log(reversed);
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      console.log(s);
+    }
+  }
+
+ console.log(reverseString("Something")) 
